ddcff0b77ada7f1d9ae0c94996e7800b67646cb5
Marsel Sidikov <sidikov.marsel@gmail.com>
2023-10-27 11:22:33 +0200

    add online_shop project

---
+++ src/test/java/de/ait/template/TemplateApplicationTests.java
@@ -0,0 +1,15 @@
+package de.ait.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class TemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
